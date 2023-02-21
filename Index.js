d509fafacef6272660a0e4dcafa401c32711203a
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-02-20 21:24:48 -0300

    Add files via upload

--- Index.js
+++ Index.js
@@ -30,14 +30,38 @@ const AdicionarEnter = Input.addEventListener('keypress', function(e){
     if (e.keyCode===13){ 
         if(!Input.value) return;
         criarItem(Input.value);
+        salvali();
     }
 })  
 
 Adicionar.addEventListener('click', function(){
-    criarItem(Input.value);    
+    criarItem(Input.value);
+    salvali();    
 });
 
 document.addEventListener('click', function(e){
     const el = e.target;
     if(el.classList.contains('Botão')) el.parentElement.remove();
+    salvali();
 });
+
+function salvali(){
+    const somaLi = Container.querySelectorAll('li');
+    const Array = []
+
+    for (let sli of somaLi){
+        sli = sli.innerText.replace('Apagar', '').trim();
+        Array.push(sli);
+    } 
+    const salvaJSN = JSON.stringify(Array);
+    localStorage.setItem('Lis',salvaJSN)
+}
+
+function retomali(){
+    const NLis = localStorage.getItem('Lis')
+    const retomaJSN = JSON.parse(NLis);
+    for(let rtmJSN of retomaJSN){
+        criarItem(rtmJSN);
+    }
+}
+retomali();
